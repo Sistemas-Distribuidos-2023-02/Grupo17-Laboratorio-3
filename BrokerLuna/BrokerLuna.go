b348c7df9bf19712d156a50cd123045e18fc7c17
charleslakes <carloslagoscortes@hotmail.com>
2023-11-20 21:55:21 -0300

    edit BrokerLuna

--- BrokerLuna/BrokerLuna.go
+++ BrokerLuna/BrokerLuna.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"google.golang.org/grpc"
 	pb "main/proto"
-	"fmt"
 	"strings"
 	"os"
 	"context"
@@ -14,81 +13,78 @@ import (
 
 var fulcrumServers []string // Lista de direcciones IP de servidores Fulcrum
 
-func RecibirMensajeInformantes(stream pb.BrokerLuna_RecibirMensajeInformantesServer) error {
-	// Recibe mensaje de Informantes y elige al azar un servidor Fulcrum para enviarle la IP de este al informante
+type server struct {
+	pb.UnimplementedOMSServer
+}
+
+func (s *server) NotifyBidirectional(stream pb.OMS_NotifyBidirectionalServer) error {
 	request, err := stream.Recv()
 	if err != nil {
 		return err
 	}
+	comandos := strings.Split(request.Message," ")
+	if len(comandos) != 6 {
+		return nil
+	}
 
-	// Selecciona un servidor Fulcrum al azar
-	fulcrumIP := obtenerServidorFulcrumAlAzar()
+	if comandos[1] == "Informante" {
+		ip := fulcrumServers[rand.Intn(len(fulcrumServers))]
+		mensaje := &pb.Response{Reply: ip}
+		if err := stream.Send(mensaje); err != nil {
+			return err
+		}
 
-	// Responde al informante con la IP del servidor Fulcrum
-	respuesta := &pb.RespuestaInformante{FulcrumIp: fulcrumIP}
-	if err := stream.Send(respuesta); err != nil {
-		return err
+	}
+	if comandos[1] == "Vanguardia" {
+		return RedireccionMensaje(request.Message,stream)
 	}
 
 	return nil
 }
 
-func RecibirMensajeVanguardia(stream pb.BrokerLuna_RecibirMensajeVanguardiaServer) error {
-	// Recibe mensaje de Vanguardia
-	request, err := stream.Recv()
+func RedireccionMensaje(msg string,stream pb.OMS_NotifyBidirectionalServer) error {
+	ip := fulcrumServers[rand.Intn(len(fulcrumServers))]
+	conn, err := grpc.Dial(ip, grpc.WithInsecure())
+    if err != nil {
+        log.Fatalf("No se pudo conectar al servidor: %v", err)
+		return err
+	}
+	client := pb.NewOMSClient(conn)
+	stream_fulcrum, err := client.NotifyBidirectional(context.Background())
 	if err != nil {
+        log.Fatalf("Error al abrir el flujo bidireccional: %v", err)
+		return err
+	}
+	mensaje_send := &pb.Request{Message: msg}
+	if err := stream_fulcrum.Send(mensaje_send); err != nil {
+        log.Fatalf("Error al enviar mensaje: %v", err)
 		return err
 	}
 
-	// Selecciona un servidor Fulcrum al azar
-	fulcrumIP := obtenerServidorFulcrumAlAzar()
-
-	// Crea una conexión gRPC con el servidor Fulcrum seleccionado
-	conn, err := grpc.Dial(fulcrumIP, grpc.WithInsecure())
+	mensaje_recv, err := stream_fulcrum.Recv()
 	if err != nil {
-		log.Fatalf("No se pudo conectar al servidor Fulcrum: %v", err)
-		return err
-	}
-	defer conn.Close()
-
-	// Crea un cliente gRPC para comunicarse con el servidor Fulcrum
-	fulcrumClient := pb.NewFulcrumClient(conn)
-
-	// Envía el mensaje de la Vanguardia al servidor Fulcrum seleccionado
-	respuestaFulcrum, err := fulcrumClient.ProcesarMensaje(context.Background(), &pb.MensajeVanguardia{Mensaje: request.Mensaje})
-	if err != nil {
-		log.Fatalf("Error al enviar mensaje a Fulcrum: %v", err)
+        log.Fatalf("Error al recibir el mensaje: %v", err)
 		return err
 	}
 
-	// Reenvía la respuesta de Fulcrum al servidor Vanguardia
-	respuestaVanguardia := &pb.RespuestaVanguardia{Respuesta: respuestaFulcrum.Respuesta}
-	if err := stream.Send(respuestaVanguardia); err != nil {
-		log.Fatalf("Error al enviar respuesta de Fulcrum a Vanguardia: %v", err)
+	mensaje := &pb.Response{Reply: mensaje_recv.Reply}
+	if err := stream.Send(mensaje); err != nil {
 		return err
 	}
-
 	return nil
-}
-
-
-func obtenerServidorFulcrumAlAzar() string {
-	// Elige al azar un servidor Fulcrum de la lista
-	index := rand.Intn(len(fulcrumServers))
-	return fulcrumServers[index]
 }
 
 func main() {
 	// Obtener las direcciones IP de los servidores Fulcrum desde las variables de entorno
-	fulcrumServers := []string{
+	fulcrumServers = []string{
 		os.Getenv("fulcrum1_server") + ":" + os.Getenv("fulcrum1_port"),
 		os.Getenv("fulcrum2_server") + ":" + os.Getenv("fulcrum2_port"),
 		os.Getenv("fulcrum3_server") + ":" + os.Getenv("fulcrum3_port"),
 	}
 
-	informante1Server := os.Getenv("informante1_server") + ":" + os.Getenv("informante1_port")
-	informante2Server := os.Getenv("informante2_server") + ":" + os.Getenv("informante2_port")
-	vanguardiaServer := os.Getenv("vanguardia_server") + ":" + os.Getenv("vanguardia_port")
+	//informante1Server := os.Getenv("informante1_server") + ":" + os.Getenv("informante1_port")
+	//informante2Server := os.Getenv("informante2_server") + ":" + os.Getenv("informante2_port")
+	//vanguardiaServer := os.Getenv("vanguardia_server") + ":" + os.Getenv("vanguardia_port")
 
 	// Inicia el servidor BrokerLuna
 	listener, err := net.Listen("tcp", "localhost:50051")
@@ -97,7 +93,7 @@ func main() {
 	}
 
 	serv := grpc.NewServer()
-	pb.RegisterBrokerLunaServer(serv, &server{})
+	pb.RegisterOMSServer(serv, &server{})
 
 	log.Printf("BrokerLuna listening at %v", listener.Addr())
 	if err := serv.Serve(listener); err != nil {
